0ea3a2e75ce2205adb0ec16289e008db56997812
KorayhanAvcu <84989228+KorayhanAvcu@users.noreply.github.com>
2024-01-11 13:39:08 +0300

    Add files via upload

---
+++ node_dariealani/alan.js
@@ -0,0 +1,9 @@
+let radius = process.argv.slice(2);
+
+let pi=3.14;
+
+function alan(pi, radius){
+    console.log("Yaricap : " + radius + " olan dairenin alani : " + (pi*(radius*radius)));
+}
+
+alan(pi,radius);
